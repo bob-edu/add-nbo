9092230c578650a846f8bc0ef0cfa06bc9cb6244
hackloud <rladydrlaqkq@gmail.com>
2021-07-16 22:44:41 +0900

    add-nbo 구현

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <netinet/in.h>
+
+uint32_t readFile(char * fileName) {
+    FILE* fp = fopen(fileName, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "[!] Cannot open the file!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uint32_t buffer;
+    size_t count = fread(&buffer, 1, 1024, fp);
+
+    fclose(fp);
+
+    return htonl(buffer);
+}
+
+int main(int argc, char *argv[])
+{
+    uint32_t num1, num2, sum;
+
+    num1 = readFile(argv[1]);
+    num2 = readFile(argv[2]);
+
+    sum = num1 + num2;
+
+    printf("%u(0x%x) + %u(0x%x) = %u(0x%x)\n", num1, num1, num2, num2, sum, sum);
+    
+    return 0;
+}
